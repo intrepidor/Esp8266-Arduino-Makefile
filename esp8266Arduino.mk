1254ebbce1bec695fdcf431d9431ed0d06d5c947
Allan <nalla@kariallan.com>
2016-02-22 20:47:10 -0600

    fixed makefile so it works for both Linux and Cygwin/windows.

--- esp8266Arduino.mk
+++ esp8266Arduino.mk
@@ -2,9 +2,17 @@
 # Tested with version 1.6.5-r5
 
 TARGET = $(notdir $(realpath .))
-ROOT_DIR_CYGWIN := $(shell dirname $(abspath $(lastword $(MAKEFILE_LIST))))
-# The extensa tools cannot use cygwin paths, so convert /cygdrive/c/abc/... to c:/cygwin64/abc/...
-ROOT_DIR := $(shell cygpath -m $(ROOT_DIR_CYGWIN))
+
+ARCH = $(shell uname)
+ifeq ($(ARCH), Linux)
+   ROOT_DIR := $(shell dirname $(abspath $(lastword $(MAKEFILE_LIST))))
+   EXEC_EXT = ""
+else
+   # The extensa tools cannot use cygwin paths, so convert /cygdrive/c/abc/... to c:/cygwin64/abc/...
+   ROOT_DIR_RAW := $(shell dirname $(abspath $(lastword $(MAKEFILE_LIST))))
+   ROOT_DIR := $(shell cygpath -m $(ROOT_DIR_RAW))
+   EXEC_EXT = ".exe"
+endif
 
 ################################################################################################
 ####
@@ -47,7 +55,7 @@ CORE_INC = $(ESP_CORES) $(ESP_HOME)/variants/$(VARIANT) $(ESP_CORES)/spiffs
 XTENSA_TOOLCHAIN ?= $(ROOT_DIR)/xtensa-lx106-elf/bin/
 
 ESPRESSIF_SDK = $(ESP_HOME)/tools/sdk
-ESPTOOL ?= $(ROOT_DIR)/bin/esptool.exe
+ESPTOOL ?= $(ROOT_DIR)/bin/esptool$(EXEC_EXT)
 ESPOTA ?= $(ESP_HOME)/tools/espota.py
 #ESPTOOL_VERBOSE ?= -vv
 
@@ -57,34 +65,8 @@ AR := $(XTENSA_TOOLCHAIN)xtensa-lx106-elf-ar
 LD := $(XTENSA_TOOLCHAIN)xtensa-lx106-elf-gcc
 OBJDUMP := $(XTENSA_TOOLCHAIN)xtensa-lx106-elf-objdump
 SIZE := $(XTENSA_TOOLCHAIN)xtensa-lx106-elf-size
-CAT	= cat.exe
-SED = sed.exe
-
-# xtensa-lx106-elf-addr2line.exe
-# xtensa-lx106-elf-ar.exe
-# xtensa-lx106-elf-as.exe
-# xtensa-lx106-elf-c++.exe
-# xtensa-lx106-elf-c++filt.exe
-# xtensa-lx106-elf-cpp.exe
-# xtensa-lx106-elf-elfedit.exe
-# xtensa-lx106-elf-g++.exe
-# xtensa-lx106-elf-gcc.exe
-# xtensa-lx106-elf-gcc-4.8.2.exe
-# xtensa-lx106-elf-gcc-ar.exe
-# xtensa-lx106-elf-gcc-nm.exe
-# xtensa-lx106-elf-gcc-ranlib.exe
-# xtensa-lx106-elf-gcov.exe
-# xtensa-lx106-elf-gprof.exe
-# xtensa-lx106-elf-ld.bfd.exe
-# xtensa-lx106-elf-ld.exe
-# xtensa-lx106-elf-nm.exe
-# xtensa-lx106-elf-objcopy.exe
-# xtensa-lx106-elf-objdump.exe
-# xtensa-lx106-elf-ranlib.exe
-# xtensa-lx106-elf-readelf.exe
-# xtensa-lx106-elf-size.exe
-# xtensa-lx106-elf-strings.exe
-# xtensa-lx106-elf-strip.exe
+CAT = cat$(EXEC_EXT)
+SED = sed$(EXEC_EXT)
 
 ################################################################################################
 ####
@@ -124,7 +106,7 @@ LOCAL_SRCS = $(USER_SRC) $(USER_CXXSRC) $(LIB_INOSRC) $(USER_HSRC) $(USER_HPPSRC
 ifndef USER_LIBS
     # automatically determine included user libraries
     USER_LIBS = $(sort $(filter $(notdir $(wildcard $(USER_LIBDIR)/*)), \
-        $(shell sed -ne 's/^ *\# *include *[<\"]\(.*\)\.h[>\"]/\1/p' $(LOCAL_SRCS))))
+        $(shell $(SED) -ne 's/^ *\# *include *[<\"]\(.*\)\.h[>\"]/\1/p' $(LOCAL_SRCS))))
 endif
 
 # user libraries and sketch code
@@ -139,7 +121,7 @@ ULIBDIRS = $(sort $(dir $(wildcard \
 ifndef ARDUINO_LIBS
     # automatically determine included libraries
     ARDUINO_LIBS = $(sort $(filter $(notdir $(wildcard $(ESP_HOME)/libraries/*)), \
-        $(shell sed -ne 's/^ *\# *include *[<\"]\(.*\)\.h[>\"]/\1/p' $(LOCAL_SRCS))))
+        $(shell $(SED) -ne 's/^ *\# *include *[<\"]\(.*\)\.h[>\"]/\1/p' $(LOCAL_SRCS))))
 endif
 
 # arduino libraries
